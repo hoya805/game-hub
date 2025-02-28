c313bf3ceb8a4bf6708960266de9397acfc08baf
Scott Kim <hoya805@gmail.com>
2025-02-28 10:56:39 -0500

    Creating custom hooks for fetching data from API calls

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,10 @@
 import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import useGames from "../hooks/useGames";
 import { Text } from "@chakra-ui/react";
 
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
+  const { games, error } = useGames();
 
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -33,5 +16,41 @@ const GameGrid = () => {
     </>
   );
 };
+
+//moved to Custom hooks.
+//   interface Game {
+//     id: number;
+//     name: string;
+//   }
+
+//   interface FetchGamesResponse {
+//     count: number;
+//     results: Game[];
+//   }
+//   const GameGrid = () => {
+
+//     };
+
+// const GameGrid = () => {
+//   const [games, setGames] = useState<Game[]>([]);
+//   const [error, setError] = useState("");
+
+//   useEffect(() => {
+//     apiClient
+//       .get<FetchGamesResponse>("/games")
+//       .then((res) => setGames(res.data.results))
+//       .catch((err) => setError(err.message));
+//   });
+//   return (
+//     <>
+//       {error && <Text>{error}</Text>}
+//       <ul>
+//         {games.map((game) => (
+//           <li key={game.id}>{game.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
 
 export default GameGrid;
