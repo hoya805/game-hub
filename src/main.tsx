29ba590f2a5a41ad40f85be22d3f1f3095a42dd7
Scott Kim <hoya805@gmail.com>
2025-02-27 20:58:22 -0500

    build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider value={null} children={null}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
