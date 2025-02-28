becf125dc1e2a51b7ac431ecad0cdc6743aef29a
Scott Kim <hoya805@gmail.com>
2025-02-27 21:19:31 -0500

    Implement dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
+import theme from "./theme";
 import "./index.css";
+import { ThemeContext } from "@emotion/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider value={null} children={null}>
+    <ChakraProvider theme={theme} value={null} children={null}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
